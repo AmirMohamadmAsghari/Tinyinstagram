3a1e9f124dc0e59c369e6e0ff6859201c1a904da
Amir <amirm2.asghari0@gmail.com>
2024-03-13 13:42:50 +0330

    create class model for profile table and follower table

---
+++ TinyInstagram/user/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class User(models.Model):
+    pass
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    followers = models.IntegerField(default=0)
+    following = models.IntegerField(default=0)
+    avatar = models.ImageField()
+    bio = models.TextField()
+
+
+class Follow(models.Model):
+    follower = models.ForeignKey(User, related_name='followers', on_delete=models.CASCADE)
+    following = models.ForeignKey(User, related_name='following', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
