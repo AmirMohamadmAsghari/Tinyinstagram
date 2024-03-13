760d4dc0d32e52be6d16555a216cc8d100350195
Amir <amirm2.asghari0@gmail.com>
2024-03-13 17:45:19 +0330

    edite user model and setting file

--- TinyInstagram/TinyInstagram/settings.py
+++ TinyInstagram/TinyInstagram/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'user',
+    'post',
 ]
 
 MIDDLEWARE = [
@@ -106,19 +108,26 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tehran'
 
-USE_I18N = True
+USE_I18N = False
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / "media/uploads"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = 'user/login_user'
+AUTH_USER_MODEL = 'user.User'
+
--- TinyInstagram/user/models.py
+++ TinyInstagram/user/models.py
@@ -1,16 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, Group, BaseUserManager
+from django.utils import timezone
 
 
-class User(models.Model):
-    pass
+class CustomUserManager(BaseUserManager):
+    def create_user(self, phone, password=None, **extra_fields):
+        if not phone:
+            raise ValueError('The Phone field must be set')
+        user = self.model(phone=phone, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, phone, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        return self.create_user(phone, password, **extra_fields)
 
 
+class User(AbstractBaseUser, PermissionsMixin):
+    name = models.CharField(max_length=150)
+    email = models.EmailField(blank=True)
+    phone = models.CharField(unique=True, max_length=11)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
+    USERNAME_FIELD = 'phone'
+    REQUIRED_FIELDS = ['name']
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.name
+
+
+groups = models.ManyToManyField(Group, verbose_name='groups', blank=True,
+                                help_text='The groups this user belongs to. A user will get all permissions granted '
+                                          'to each of their groups.',
+                                related_name="user_set", related_query_name="user")
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, related_name='profile')
     followers = models.IntegerField(default=0)
     following = models.IntegerField(default=0)
     avatar = models.ImageField(upload_to='avatars/', blank=True)
-    bio = models.TextField(blank=True,null=True)
+    bio = models.TextField(blank=True, null=True)
 
 
 class Follow(models.Model):
