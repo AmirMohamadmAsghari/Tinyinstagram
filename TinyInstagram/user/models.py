1083b870b674c5173cd1f9fff75b7895f83854ea
Amir <amirm2.asghari0@gmail.com>
2024-03-13 13:47:31 +0330

    create class model for Post table in Post app

--- TinyInstagram/user/models.py
+++ TinyInstagram/user/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class User(models.Model):
     pass
 
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     followers = models.IntegerField(default=0)
