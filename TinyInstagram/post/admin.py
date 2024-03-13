936d8c7c7d85f0c73cb0728428e5768e3a1171f6
Amir <amirm2.asghari0@gmail.com>
2024-03-13 17:49:07 +0330

    edite user admin file in user app and post app

--- TinyInstagram/post/admin.py
+++ TinyInstagram/post/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Post, Reply, Like, Dislike, Images, Tag, Comment
 
-# Register your models here.
+admin.site.register(Post)
+admin.site.register(Reply)
+admin.site.register(Like)
+admin.site.register(Dislike)
+admin.site.register(Images)
+admin.site.register(Tag)
+admin.site.register(Comment)
+
