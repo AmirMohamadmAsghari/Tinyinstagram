37adee74e62e940359d1be93c3392f6658fac65e
Amir <amirm2.asghari0@gmail.com>
2024-03-13 14:49:19 +0330

    create OTP models

--- TinyInstagram/user/models.py
+++ TinyInstagram/user/models.py
@@ -9,11 +9,17 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     followers = models.IntegerField(default=0)
     following = models.IntegerField(default=0)
-    avatar = models.ImageField()
-    bio = models.TextField()
+    avatar = models.ImageField(upload_to='avatars/', blank=True)
+    bio = models.TextField(blank=True,null=True)
 
 
 class Follow(models.Model):
     follower = models.ForeignKey(User, related_name='followers', on_delete=models.CASCADE)
     following = models.ForeignKey(User, related_name='following', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+class OTP(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    otp_code = models.IntegerField()
+    expiration_time = models.DateTimeField()
