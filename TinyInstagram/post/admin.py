2674e0f9e86c22e63865876652d7f5590dd2c932
Amir <amirm2.asghari0@gmail.com>
2024-03-23 18:17:05 +0330

    Writing post program admin settings

--- TinyInstagram/post/admin.py
+++ TinyInstagram/post/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Post, Reply, Like, Dislike, Images, Tag, Comment
+from .models import Post, Images, Tag, Comment, Dislike, Like
 
 admin.site.register(Post)
-admin.site.register(Reply)
+# admin.site.register(Reply)
 admin.site.register(Like)
 admin.site.register(Dislike)
 admin.site.register(Images)
