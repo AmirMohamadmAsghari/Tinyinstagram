55640e4a009238b9b287409822b0222df6a2470e
Amir <amirm2.asghari0@gmail.com>
2024-04-02 18:22:27 +0330

    New changes in views user

--- TinyInstagram/user/views.py
+++ TinyInstagram/user/views.py
@@ -6,6 +6,8 @@ from django.contrib import messages
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.hashers import make_password
+from django.utils import timezone
+from .utils import generate_otp_code, send_email
 
 
 # Create your views here.
@@ -40,14 +42,40 @@ def login_user(request):
 
         user = authenticate(request, phone=phone, password=password)
         if user is not None:
-            login(request, user)
-            messages.success(request, 'Your login was successful!')
-            return redirect('home')
+
+            otp_code = generate_otp_code()
+
+            user.otp_code = otp_code
+            user.save()
+
+            send_email(user.email, otp_code)
+
+            return redirect('verify_otp')
         else:
             messages.error(request, 'The phone number or password is incorrect. Please try again.')
             return redirect('login')
 
     return render(request, 'login.html')
+
+
+def verify_otp(request):
+    if request.method == 'POST':
+        otp_code = request.POST.get('otp_code')
+        phone = request.POST.get('phone')  # Retrieve phone number from the form
+        print(phone, otp_code)
+        user = User.objects.filter(phone=phone).first()
+        if user:
+            print(user.phone, user.otp_code)
+            if str(user.otp_code).strip() == str(otp_code).strip():
+                login(request, user)  # Authenticate the user here
+                messages.success(request, 'Your login was successful!')
+                return redirect('home')
+            else:
+                messages.error(request, 'Invalid OTP code.')
+        else:
+            messages.error(request, 'User does not exist.')
+        return redirect('login')
+    return render(request, 'otp_verification.html')
 
 
 def logout_user(request):
@@ -83,31 +111,84 @@ def user_profile(request, user_id):
     return render(request, 'user_profile.html', {"user": user,'profile': profile})
 
 
+# @login_required
+# def follow_user(request, user_id):
+#     if request.method == 'POST' and request.is_ajax():
+#         # Check if the user exists and is not the same as the logged-in user
+#         try:
+#             user_to_follow = User.objects.get(id=user_id)
+#             if user_to_follow == request.user:
+#                 return JsonResponse({'success': False, 'error': 'You cannot follow yourself'})
+#         except User.DoesNotExist:
+#             return JsonResponse({'success': False, 'error': 'User does not exist'})
+#
+#         # Check if the user is already being followed
+#         if not Follow.objects.filter(follower=request.user, following=user_to_follow).exists():
+#             # If not, create a new Follow object
+#             Follow.objects.create(follower=request.user, following=user_to_follow)
+#             # Update follower count for both users
+#             request.user.profile.following += 1
+#             request.user.profile.save()
+#             user_to_follow.profile.followers += 1
+#             user_to_follow.profile.save()
+#             return JsonResponse({'success': True})
+#         else:
+#             return JsonResponse({'success': False, 'error': 'You are already following this user'})
+#     else:
+#         return JsonResponse({'success': False, 'error': 'Invalid request method'})
 @login_required
-def follow_user(request, user_id):
-    if request.method == 'POST' and request.is_ajax():
-        # Check if the user exists and is not the same as the logged-in user
-        try:
-            user_to_follow = User.objects.get(id=user_id)
-            if user_to_follow == request.user:
-                return JsonResponse({'success': False, 'error': 'You cannot follow yourself'})
-        except User.DoesNotExist:
-            return JsonResponse({'success': False, 'error': 'User does not exist'})
+def follow_user(request, profile_id, action):
+    if action == 'follow':
+        if request.method == 'POST':
+            profile = get_object_or_404(Profile, pk=profile_id)
+            follower_profile = request.user.profile
+            follow_instance, created = Follow.objects.get_or_create(follower=follower_profile.user,
+                                                                    following=profile.user)
+            if created:
+                # Increase the number of followers for the user being followed (Ali)
+                profile.followers += 1
+                profile.save()
+                # Increase the number of followings for the follower (Amir)
+                follower_profile.following += 1
+                follower_profile.save()
+                return JsonResponse({'status': 'success', 'action': 'followed', 'following_count': profile.followers})
+            else:
+                return JsonResponse({'status': 'error', 'message': 'You are already following this user.'})
+    elif action == 'unfollow':
+        if request.method == 'POST':
+            profile = get_object_or_404(Profile, pk=profile_id)
+            follower_profile = request.user.profile
+            follow_instance = Follow.objects.filter(follower=follower_profile.user, following=profile.user).first()
+            if follow_instance:
+                follow_instance.delete()
+                # Decrease the number of followers for the user being unfollowed (Ali)
+                profile.followers -= 1
+                profile.save()
+                # Decrease the number of followings for the follower (Amir)
+                follower_profile.following -= 1
+                follower_profile.save()
+                return JsonResponse({'status': 'success', 'action': 'unfollowed', 'following_count': profile.followers})
+            else:
+                return JsonResponse({'status': 'error', 'message': 'You are not following this user.'})
 
-        # Check if the user is already being followed
-        if not Follow.objects.filter(follower=request.user, following=user_to_follow).exists():
-            # If not, create a new Follow object
-            Follow.objects.create(follower=request.user, following=user_to_follow)
-            # Update follower count for both users
-            request.user.profile.following += 1
-            request.user.profile.save()
-            user_to_follow.profile.followers += 1
-            user_to_follow.profile.save()
-            return JsonResponse({'success': True})
-        else:
-            return JsonResponse({'success': False, 'error': 'You are already following this user'})
-    else:
-        return JsonResponse({'success': False, 'error': 'Invalid request method'})
+    return JsonResponse({'status': 'error', 'message': 'Invalid action or request method.'}, status=400)
+
+# @login_required
+# def unfollow_user(request, profile_id):
+#     if request.method == 'POST':
+#         profile = get_object_or_404(Profile, pk=profile_id)
+#         follower = request.user.profile
+#         follow_instance = Follow.objects.filter(follower=follower, following=profile.user).first()
+#         if follow_instance:
+#             follow_instance.delete()
+#             profile.followers -= 1
+#             profile.save()
+#             return JsonResponse({'status': 'success', 'action': 'unfollowed'})
+#         else:
+#             return JsonResponse({'status': 'error', 'message': 'You are not following this user.'})
+
+
+
 
 
 def search_user(request):
